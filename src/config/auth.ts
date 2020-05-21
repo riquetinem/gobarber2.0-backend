7c5053aa9a1d6772958877f0e31733e9b94227c5
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2020-05-21 16:43:48 -0300

    utilizando o class transformer

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: 'senhamegahyperblasterseguraaquidentro',
+    secret: process.env.APP_SECRET,
     expiresIn: '7d',
   },
 };
--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -5,6 +5,7 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Exclude, Expose } from 'class-transformer';
 
 // KISS - Keep It Simple & Stupid
 
@@ -20,6 +21,7 @@ class User {
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
 
   @Column()
@@ -30,6 +32,13 @@ class User {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  @Expose({ name: 'avatar_url' })
+  getAvatarUrl(): string | null {
+    return this.avatar
+      ? `${process.env.APP_API_URL}/files/${this.avatar}`
+      : null;
+  }
 }
 
 export default User;
