f99840cf44142bba4bfbcc64b907597ea4321a95
Henrique Shinhate Tinem <riquetinem@pop-os.localdomain>
2020-07-22 11:16:36 -0300

    ajustes no backend

--- src/modules/appointments/services/ListProviderService.ts
+++ src/modules/appointments/services/ListProviderService.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 // import AppError from '@shared/errors/AppError';
@@ -29,7 +30,10 @@ class ListProviderService {
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
